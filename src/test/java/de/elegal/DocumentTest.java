b0b5fefc22c9265bcb75243bf0b9e5114ec526c9
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2019-07-29 23:00:43 +0300

    added throws to DocumentTest

--- src/test/java/de/elegal/DocumentTest.java
+++ src/test/java/de/elegal/DocumentTest.java
@@ -5,14 +5,16 @@ import org.junit.Test;
 public class DocumentTest {
     @Test
     public void replace() {
-
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     @Test
     public void isReplaceable() {
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     @Test
     public void getStringMap() {
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 }
