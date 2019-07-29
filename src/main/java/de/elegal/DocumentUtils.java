f841ac4127dba23a8061e87da4658bd06f1dc187
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2019-07-29 20:37:38 +0200

    DocumentUtils minor improvements

--- src/main/java/de/elegal/DocumentUtils.java
+++ src/main/java/de/elegal/DocumentUtils.java
@@ -5,14 +5,13 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import org.apache.poi.xwpf.usermodel.*;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
-// TODO: Multiple Ocurrences
-
 public class DocumentUtils {
+  public final static String OPENING_TAG = "<";
+  public final static String CLOSING_TAG = ">";
+
   public static XWPFDocument openFile(final String str) throws NullPointerException, InvalidFormatException, IOException {
     Objects.requireNonNull(str);
     File file = new File(str);
@@ -27,29 +26,36 @@ public class DocumentUtils {
     doc.write(new FileOutputStream(str));
   }
 
+  public static Tuple<Integer, XWPFDocument> replaceAllTags(XWPFDocument doc, final String tag, final String replace) {
+    String newTag = OPENING_TAG + tag + CLOSING_TAG;
+    return replaceAllStrings(doc, newTag, replace);
+  }
+
   // Not working with tables
   // If needed, should look sth like that https://stackoverflow.com/a/22269035
-  // Also, it allows to replace multiple occurences
-  // TODO: Currently returns XWPFDocument via Reference
-  public static boolean replaceText(XWPFDocument doc, final String tag, final String replace) {
-    if(tag == null) return false;
+  // Also, it allows to replace multiple occurrences
+  public static Tuple<Integer, XWPFDocument> replaceAllStrings(XWPFDocument doc, final String tag, final String replace) {
+    if(tag == null) return new Tuple<>(0, doc); // No replaces
     Objects.requireNonNull(doc);
     Objects.requireNonNull(replace);
-    boolean tagFound = false;
-     List<XWPFRun> debug = doc.getParagraphs()
-             .stream()
-             .map(XWPFParagraph::getRuns)
-             .filter(Objects::nonNull)
-             .collect(ArrayList::new, ArrayList::addAll, ArrayList::addAll);
-    for (XWPFRun r : debug) {
-      String text = r.getText(0);
-      if(text != null && text.contains(tag)) {
-        System.out.println(text);
-        text = text.replace(tag, replace);
-        r.setText(text, 0);
-        tagFound = true;
-      }
+    int counter = 0;
+    List<XWPFRun> runs = doc.getParagraphs()
+            .stream()
+            .map(XWPFParagraph::getRuns)
+            .filter(Objects::nonNull)
+            .collect(ArrayList::new, ArrayList::addAll, ArrayList::addAll);
+    for(XWPFRun run : runs) {
+      if (replaceRun(run, tag, replace)) counter++;
     }
-    return tagFound;
+    return new Tuple<>(counter, doc);
+  }
+
+  private static boolean replaceRun(XWPFRun run, final String tag, final String replace) {
+    if (run == null || tag == null || replace == null) return false;
+    String text= run.getText(0);
+    if (text == null || !text.contains(tag)) return false;
+    text = text.replace(tag, replace);
+    run.setText(text, 0);
+    return true;
   }
 }
