1203464009c8f59eeeb8304164ab30ee1614652c
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2019-07-29 23:23:16 +0200

    Replace Tests

--- src/test/java/de/elegal/DocumentUtilsReplaceTest.java
+++ src/test/java/de/elegal/DocumentUtilsReplaceTest.java
@@ -1,4 +1,54 @@
 package de.elegal;
 
-public class DocumentUtilsReplaceTest {
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+
+public class DocumentUtilsReplaceTest extends DocumentUtilsTest {
+
+    @Test
+    public void newHeadLine() throws Exception {
+        XWPFDocument doc = createDocument(TEST_FILE);
+        DocumentUtils.replaceAllStrings(doc, "Headline", "Hehedline");
+        doc = saveAndLoadAgain(doc, TEST_PATH + "HeadLineTest.docx");
+        List<String> allStrings = getAllStrings(doc);
+        Assert.assertEquals(1, numberOfDeepChecks(allStrings, "Hehedline"));
+    }
+
+    @Test
+    public void oldHeadLineMissing() throws Exception {
+        XWPFDocument doc = createDocument(TEST_FILE);
+        DocumentUtils.replaceAllStrings(doc, "Headline", "Hehedline");
+        doc = saveAndLoadAgain(doc, TEST_PATH + "HeadLineTest.docx");
+        List<String> allStrings = getAllStrings(doc);
+        Assert.assertEquals(0, numberOfDeepChecks(allStrings, "Headline"));
+    }
+
+    @Test
+    public void newTag() throws Exception {
+        XWPFDocument doc = createDocument(TEST_FILE);
+        DocumentUtils.replaceAllTags(doc, "Tag", "Yeah");
+        doc = saveAndLoadAgain(doc, TEST_PATH + "HeadLineTest.docx");
+        List<String> allStrings = getAllStrings(doc);
+        for (String x : allStrings) {
+            System.out.println(x);
+        }
+        Assert.assertEquals(1, numberOfDeepChecks(allStrings, "Yeah"));
+    }
+
+    @Test
+    public void oldTagMissing() throws Exception {
+        XWPFDocument doc = createDocument(TEST_FILE);
+        DocumentUtils.replaceAllTags(doc, "Tag", "Yeah");
+        doc = saveAndLoadAgain(doc, TEST_PATH + "HeadLineTest.docx");
+        List<String> allStrings = getAllStrings(doc);
+        for (String x : allStrings) {
+            System.out.println(x);
+        }
+        Assert.assertEquals(0, numberOfDeepChecks(allStrings,
+                DocumentUtils.OPENING_TAG + "Tag" + DocumentUtils.CLOSING_TAG));
+    }
 }
--- src/test/java/de/elegal/DocumentUtilsTest.java
+++ src/test/java/de/elegal/DocumentUtilsTest.java
@@ -1,4 +1,59 @@
 package de.elegal;
 
-public class DocumentUtilsTest {
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class DocumentUtilsTest {
+    protected static final String TEST_PATH = System.getProperty("user.dir")+ "/src/test/resources/docx/";
+    protected static final String TEST_FILE = TEST_PATH + "Headline.docx";
+
+    protected XWPFDocument createDocument(String fileName) throws Exception{
+        return DocumentUtils.openFile(fileName);
+    }
+
+    protected List<String> getAllStrings(String fileName) throws Exception {
+        XWPFDocument doc = createDocument(fileName);
+        return getAllStrings(doc);
+    }
+
+    // Needed for reflections
+    @SuppressWarnings("unchecked")
+    protected List<String> getAllStrings(XWPFDocument document) {
+        // We really don't want to make the method public since its not for normal usage
+        // therefore we use reflections.
+        List<XWPFRun> allElements;
+        try {
+            Method getRunsFromDocument = DocumentUtils.class
+                    .getDeclaredMethod("getRunsFromDocument", XWPFDocument.class);
+            getRunsFromDocument.setAccessible(true);
+            allElements = (List<XWPFRun>) getRunsFromDocument.invoke(null, document);
+
+        } catch (Exception e) {
+            throw new IllegalStateException("This should never happen. The test itself failed.");
+        }
+
+        List<String> allStrings = allElements.stream().map(run -> run.getText(0)).collect(Collectors.toList());
+        return allStrings;
+    }
+
+    protected int numberOfDeepChecks(List<String> strings, String subString) {
+        int i = 0;
+        for (String str : strings) {
+            if (str.contains(subString)) i++;
+        }
+        return i;
+    }
+
+    protected XWPFDocument saveAndLoadAgain(XWPFDocument toBeSaved, String path) throws Exception{
+        // If it doesn't exist, thats okay too. We just don't want to fail it because the file is already existing
+        new File(path).delete();
+
+        DocumentUtils.writeFile(toBeSaved, path);
+        return createDocument(path);
+    }
 }
