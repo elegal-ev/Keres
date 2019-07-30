97c7e1f5c03057ce6113e3d12de4066e64f1fb17
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2019-07-30 03:49:46 +0300

    TDD purposely failing Test Classes

---
+++ src/test/java/de/elegal/CalculatorTest.java
@@ -0,0 +1,12 @@
+package de.elegal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class CalculatorTest {
+    @Test
+    public void test() {
+        fail("No tests provided");
+    }
+}
---
+++ src/test/java/de/elegal/EmployeeTest.java
@@ -0,0 +1,12 @@
+package de.elegal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class EmployeeTest {
+    @Test
+    public void test() {
+        fail("No tests provided");
+    }
+}
